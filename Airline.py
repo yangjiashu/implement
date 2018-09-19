c6415b2b33c6ce3ab2e0d8664d69ebcdcfbc82ea
yangjiashu <1098325805@qq.com>
2018-09-19 15:07:15 +0800

    '有流程图'

--- Airline.py
+++ Airline.py
@@ -4,43 +4,22 @@ import numpy as np
 import gym
 
 class Airline(gym.Env):
-    def __init__(self, capacity, time):
+    def __init__(self, capacity, times):
         '''
         :param capacity: An integer, representing the total capacity of the airplane
         :param time: An ndarray, a set of time horizon
         '''
         self.capacity = capacity
-        self.time = time
+        self.times = times
 
-        self.left_c = capacity #现在的剩余座位数是总容量
-        self.left_t = time[len(time)-1] #现在的剩余时间是销售时间
-        self.i = 0 #time数组的下标
+        self.left_c = capacity
+        self.i = 0
+        self.left_t = self.times[self.i]
+
     def reset(self):
         self.left_c = self.capacity
         self.i = 0
-        self.left_t = self.time[self.i]
+        self.left_t = self.times[self.i]
 
-        return (self.left_c, self.left_t)
+        return tuple([self.left_c, self.left_t])
 
-    def step(self, action):
-        assert isinstance(action, int)
-
-        order = self.demmand_func(self.left_t, action)
-        #terminal
-        if (self.left_c - order) <= 0 or self.i == len(self.time)-1:
-            left_c_ = max(0, self.left_c-order)
-            #return state, reward, done, info
-            return (self.left_c, self.left_t), min(order, self.left_c) * action, \
-                    1, {}
-
-        #not terminal
-        else:
-            left_c_ = self.left_c - order #求出s_
-            self.left_c = left_c_ # 令s = s_
-            self.i += 1
-            self.left_t = self.time[self.i]
-            return (self.left_c, self.left_t), order * action, \
-                    0, {}
-
-    def demmand_func(self, t, a):
-        return 75-5*t*np.exp(-2/t * a/100)
--- main.py
+++ main.py
@@ -3,37 +3,3 @@ from Airline import Airline
 from RL import Q_lam
 import numpy as np
 
-def update(env, RL, iter_times):
-    for i in range(iter_times):
-        RL.e_table *= 0
-        s = env.reset()
-        a = RL.e_greedy(s)
-
-        while True:
-            alpha = 1 / (i + 2)
-            s_, r, done, info = env.step(a)
-            s, a = RL.learn(done, s, a, r, s_, alpha)
-
-            if done:
-                break
-
-        if i % 50 == 0:
-            print(RL.q_table)
-
-
-if __name__ == '__main__':
-    capacity = 5
-    time = [10, 7, 4, 1]
-
-    states = []
-    for i in range(capacity):
-        for t in time:
-            states.append(tuple([i+1,t]))
-    print(states)
-    prices = np.linspace(70, 120, 3)
-    iter_times = 1000
-
-    env = Airline(capacity, time)
-    RL = Q_lam(states,prices)
-
-    update(env, RL, iter_times)
